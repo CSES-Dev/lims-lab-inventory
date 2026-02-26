ce2d4960cde969193a88652b51c261be71007012
davdwan21 <141950513+davdwan21@users.noreply.github.com>
2026-02-25 16:12:56 -0800

    Adjustments to models, services, and general APIs
    + General route.ts no longer uses a cached connection. Also returns NextResponse upon failed connection attempt.
    + GET and POST now use new connection attempt
    + POST utilizes safeParse instead of parse to validate body with schema.
    + Model exports cleaned up and condensed.
    + Services no longer attempt to connect to database
    + Comment descriptor for each method
    + Methods no longer throw errors, now simply return null upon nonexistent item
    
    Next steps:
    - implement getFiltered service and API
    - fix id specific routing

--- app/api/inventory/route.ts
+++ app/api/inventory/route.ts
@@ -6,8 +6,19 @@ import {
     notificationEventValues,
 } from "@/models/Item";
 import { addItem, getItems } from "@/services/items";
+import { connectToDatabase } from "@/lib/mongoose";
 
-// REST APIs split among route.ts and [id]/route.ts for cleaner management
+// Only returns a Next response upon failed connection
+async function connect() {
+    try {
+        await connectToDatabase();
+    } catch {
+        return NextResponse.json(
+            { success: false, message: "Error connecting to database" },
+            { status: 500 }
+        );
+    }
+}
 
 const thresholdSchema = z.object({
     minQuantity: z.number().int().nonnegative(),
@@ -30,8 +41,12 @@ const itemCreateSchema = z.object({
 });
 
 // GET: fetch all items
-// implement page, limit, labid search params
 export async function GET() {
+    const connectionResponse = await connect();
+    if (connectionResponse) {
+        return connectionResponse;
+    }
+
     try {
         const items = await getItems();
         return NextResponse.json(items, { status: 200 });
@@ -43,15 +58,34 @@ export async function GET() {
     }
 }
 
+// GET FILTERED should go here
+
 // POST: add a new item
 export async function POST(request: Request) {
+    const connectionResponse = await connect();
+    if (connectionResponse) {
+        return connectionResponse;
+    }
+
+    const body = await request.json();
+    const parsedBody = itemCreateSchema.safeParse(body);
+
+    if (!parsedBody.success) {
+        return NextResponse.json(
+            {
+                success: false,
+                message: "Invalid request body.",
+            },
+            { status: 400 }
+        );
+    }
+
     try {
-        const newItem = itemCreateSchema.parse(await request.json());
-        const created = await addItem(newItem);
+        const created = await addItem(parsedBody.data);
         return NextResponse.json(created, { status: 201 });
     } catch {
         return NextResponse.json(
-            { message: "Failed to fetch items" },
+            { message: "Error occured while creating item" },
             { status: 500 }
         );
     }
