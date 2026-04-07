b460431c32dc8fe2ebcd031172f32f3e5aa4ab9b
Amormio Velasquez III <amorbuks25@gmail.com>
2026-04-07 08:14:38 -0700

    Updating jest config file to match main branch

--- jest.config.ts
+++ jest.config.ts
@@ -24,15 +24,15 @@ const config: Config = {
 
   // Coverage settings
   collectCoverageFrom: [
-    "services/listings/listings.ts", // Include the service file for listings
-    "app/api/listings/**/*.ts", // Include all route files for the listings API
-    "lib/**/mongoose.ts", // include the mongoose test given
-    "!app/api/listings/**/__tests__/**/*.ts", // Exclude test files in the listings API
-    "!services/listings/**/__tests__/**/*.ts", // Exclude test files in the listings service
-    "!**/*.d.ts", // Exclude type declaration files
-    "!**/node_modules/**", // Exclude node_modules
-    "!**/.next/**", // Exclude Next.js build files
-    "!**/coverage/**", // Exclude coverage files
+    "app/*/.ts",
+    "app/*/.tsx",
+    "services/*/.ts",
+    "models/*/.ts",
+    "lib/*/.ts",
+    "!*/.d.ts",
+    "!*/node_modules/*",
+    "!*/.next/*",
+    "!*/coverage/*",
   ],
 
   coveragePathIgnorePatterns: ["/node_modules/", "/.next/", "/coverage/"],
